d7f2be21b1f99d03a0ca3f0858322bd81f00b340
gusen <gusen1982@gmail.com>
2016-04-25 15:13:14 +0800

    add test session operation functions

--- demo/controller/index.php
+++ demo/controller/index.php
@@ -10,6 +10,7 @@ class index
 	{
 		jiashu::loadCustomLib('GenUniqID');//load custom function file.读取一个自定义的函数文件
 		JSFW()->setTplData('n',GenUniqID());
+		
 		//echo 'hello world';
 		JSFW()->render('index');
 	}
@@ -19,17 +20,33 @@ class index
 		$n = JSFW()->getConfig()['appName'];
 		$v = JSFW()->getVersion();
 		JSFW()->setTplData('n',$n)->setTplData('v',$v);
-		JSFW()->render('index');//$GLOBALS['JSFW']->render('index');
+		JSFW()->render('index');
 	}
 
 	public function db()
 	{
 		jiashu::loadLib('model');
 		$user = new model('user','testdb');
-		//$r = $user->executeSql('select * from test_user');
+		//$r = $user->executeSql('select * from test_user');//you can use executeSql() to query a SQL statment directly.用executeSql()函数可以直接执行SQL语句
 		$r = $user->field('username,email')->limit('1')->query();
-		//print_r($r);
 		JSFW()->setTplData('r',$r);
+		JSFW()->render('index');
+	}
+	
+	public function testsession()//set a session value.设置一个session值
+	{
+		jiashu::loadLib('session');
+		$sess = new session;
+		$sess->sessionStart()->setSession('testsession','session is work!');
+		JSFW()->render('index');
+	}
+	
+	public function testsession2()//get session value.读取session值看能不能得到之前设置的内容
+	{
+		$v = JSFW()->getVersion();
+		jiashu::loadLib('session');
+		$sess = new session;
+		JSFW()->setTplData('n',$sess->sessionStart()->getSession('testsession'))->setTplData('v',$v);
 		JSFW()->render('index');
 	}
 }
